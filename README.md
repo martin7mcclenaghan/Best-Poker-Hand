b2e07fcfaa8d5f71f4e5e5353eae0f3227464898
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-22 19:05:14 +0100

    Initial project setup.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Welcome to my Texas Hold'em guessing game!
+
+This is being built with Java 11 and Spring 2.1.6.
+
+The object of the game is to guess whether, based upon your cards and the communal
+cards you can see, if you have the best hand vs your opponent.
---
+++ src/main/java/learning/example/BestPokerHandApplication.java
@@ -0,0 +1,13 @@
+package learning.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestPokerHandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestPokerHandApplication.class, args);
+	}
+
+}
