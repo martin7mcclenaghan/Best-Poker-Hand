9b4724710763100955371b5d585116f28a0bbcee
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-25 19:53:28 +0100

    Added main to web module and re-organised dependencies.

--- console/src/main/java/learning/example/ConsoleMain.java
+++ console/src/main/java/learning/example/ConsoleMain.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BestPokerHandApplication {
+public class ConsoleMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BestPokerHandApplication.class, args);
+		SpringApplication.run(ConsoleMain.class, args);
 	}
 
 }
--- console/src/test/java/learning/example/ConsoleMainTests.java
+++ console/src/test/java/learning/example/ConsoleMainTests.java
@@ -1,4 +1,4 @@
-package learning.example.BestPokerHand;
+package learning.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BestPokerHandApplicationTests {
+public class ConsoleMainTests {
 
 	@Test
 	public void contextLoads() {
