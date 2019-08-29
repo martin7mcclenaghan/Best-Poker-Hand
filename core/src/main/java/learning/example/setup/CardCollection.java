8532db7a65bcfbd7718efc4f69313d9e23532ed4
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-29 13:14:13 +0100

    Abstract class added to gather functionality for creation of groups of cards into one place.

---
+++ core/src/main/java/learning/example/setup/CardCollection.java
@@ -0,0 +1,33 @@
+package learning.example.setup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class CardCollection {
+
+    // == instance variables
+    private List<Card> cards;
+
+    // == constructor ==
+    CardCollection(int size) {
+        this.cards = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            this.cards.add(Deck.getInstance().dealCard());
+        }
+    }
+
+    // == instance methods ==
+    String getCardsString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Card card : this.cards) {
+            stringBuilder.append(card.getDescription());
+            stringBuilder.append(" ");
+        }
+
+        return stringBuilder.toString();
+    }
+
+    List<Card> getCards (){
+        return this.cards;
+    }
+}
