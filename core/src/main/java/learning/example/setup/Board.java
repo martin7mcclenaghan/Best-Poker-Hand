e274d2f6772240d2cea1ecb9d9e35d04b291c4de
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-30 23:09:39 +0100

    Changed Board class to package-private.

--- core/src/main/java/learning/example/setup/Board.java
+++ core/src/main/java/learning/example/setup/Board.java
@@ -1,12 +1,12 @@
 package learning.example.setup;
 
 //Singleton Design Pattern used as there can only be one board
-public class Board extends CardCollection {
+class Board extends CardCollection {
 
     // == static members ==
     private static Board instance;
 
-    public synchronized static Board getInstance() {
+    synchronized static Board getInstance() {
 
         if (instance == null) {
             instance = new Board();
@@ -22,7 +22,7 @@ public class Board extends CardCollection {
 
     // == instance methods ==
     // method to add card to the board
-    public void addCardToBoard() {
+    void addCardToBoard() {
         if (this.getCards().size() >= 5) {
             System.out.println("Board can only contain 5 cards! Cannot add anymore!");
         } else {
