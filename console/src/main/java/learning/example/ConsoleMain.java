0af3adfa0d8ef5a3dc749cb3002c5cb34d3408d7
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-09-15 16:29:24 +0100

    Will build console main to run without Spring initially as is not strictly necessary until creation of web module.

--- console/src/main/java/learning/example/ConsoleMain.java
+++ console/src/main/java/learning/example/ConsoleMain.java
@@ -1,13 +1,29 @@
 package learning.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import learning.example.setup.Game;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@SpringBootApplication
 public class ConsoleMain {
 
+	// == constants ==
+	private static final Logger log = LoggerFactory.getLogger(ConsoleMain.class);
+
+	// == fields ==
+	private Game game;
+
+	// == constructor == 
+	public ConsoleMain(Game game) {
+		this.game = game;
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(ConsoleMain.class, args);
+
+
+
+
+
+
 	}
 
 }
