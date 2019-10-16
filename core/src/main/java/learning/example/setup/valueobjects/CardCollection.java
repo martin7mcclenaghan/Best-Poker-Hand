b8ebc7d7cf6ca211249f96da34c14a963a32308e
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-10-16 02:50:47 +0100

    Redundant code removed from CardCollection.

--- core/src/main/java/learning/example/setup/valueobjects/CardCollection.java
+++ core/src/main/java/learning/example/setup/valueobjects/CardCollection.java
@@ -11,17 +11,9 @@ public abstract class CardCollection {
 
     // == constructor ==
 
-    //TODO inject List<Card> of into Card Collection Constructor, cards will then be dealt from GameService
-
     public CardCollection(int maxSize) {
         this.cards = new ArrayList<>();
         this.maxSize = maxSize;
-
-        //leave in until can be safely deleted
-        //Service then deals cards as part of assemble game method
-//        for (int i = 0; i < size; i++) {
-//            this.cards.add(dealer.dealCard());
-//        }
     }
 
     // == instance methods ==
