0c3c4d0b3c35767f181d710a70018ded11c6d12e
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-10-16 02:27:35 +0100

    Functionality to deal a card added to GameService from DeckService. DeckService will be made redundant and removed.

--- core/src/main/java/learning/example/setup/GameService.java
+++ core/src/main/java/learning/example/setup/GameService.java
@@ -1,6 +1,7 @@
 package learning.example.setup;
 
 import learning.example.setup.valueobjects.Board;
+import learning.example.setup.valueobjects.Card;
 import learning.example.setup.valueobjects.Deck;
 import learning.example.setup.valueobjects.HoleCards;
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class GameService {
 
@@ -31,6 +33,16 @@ public class GameService {
         this.opponentHole = opponentHole;
         this.playerPossibleHands = new ArrayList<>();
         this.opponentPossibleHands = new ArrayList<>();
+    }
+
+    // == private methods ==
+    private Card getCardFromDeck(){
+        Random random = new Random();
+        int cardNumber = random.nextInt(this.deck.getCards().size());
+        Card cardDealt = this.deck.getCards().get(cardNumber);
+        this.deck.getCards().remove(cardDealt);
+        return cardDealt;
+
     }
 
     // == public methods ==
