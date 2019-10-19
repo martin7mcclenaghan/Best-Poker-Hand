1610ec1daad77b3ebe2a047db93cc14e809b785d
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-10-19 23:55:16 +0100

    Reference for DeckFormatter re-named to make more sense. Creation of DeckService object removed as is no longer needed.

--- core/src/main/java/learning/example/setup/GameAssembler.java
+++ core/src/main/java/learning/example/setup/GameAssembler.java
@@ -1,6 +1,5 @@
 package learning.example.setup;
 
-import learning.example.setup.processingobjects.DeckService;
 import learning.example.setup.processingobjects.DeckFormatter;
 import learning.example.setup.valueobjects.Board;
 import learning.example.setup.valueobjects.Deck;
@@ -11,9 +10,8 @@ public class GameAssembler {
     // == public methods ==
     public GameService assembleGame() {
 
-        DeckFormatter deckProcessor = new DeckFormatter();
-        Deck deck = new Deck(deckProcessor.getStandard52Cards());
-        DeckService steve = new DeckService("Steve", deck);
+        DeckFormatter deckFormatter = new DeckFormatter();
+        Deck deck = new Deck(deckFormatter.getStandard52Cards());
         Board board = new Board();
 
         //HoleCards must be specified as an implementation to stop players dealing themselves more cards
