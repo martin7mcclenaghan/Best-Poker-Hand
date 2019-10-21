247e462cf96f8ed314aa9e93efc4c67f3f6f739f
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-10-21 23:59:23 +0100

    processing removed from Board class.

--- core/src/main/java/learning/example/setup/valueobjects/Board.java
+++ core/src/main/java/learning/example/setup/valueobjects/Board.java
@@ -1,26 +1,9 @@
 package learning.example.setup.valueobjects;
 
-import learning.example.setup.processingobjects.DeckService;
-
 public class Board extends CardCollection {
-
-    private DeckService dealer;
 
     public Board() {
         super(3);
     }
-
-    // == instance methods ==
-
-    // TODO this needs to be taken care of in the Game Service
-    // method to add card to the board
-    public void addCardToBoard() {
-        if (this.getCards().size() >= 5) {
-            System.out.println("Board can only contain 5 cards! Cannot add anymore!");
-        } else {
-            this.getCards().add(this.dealer.dealCard());
-        }
-    }
-
 
 }
